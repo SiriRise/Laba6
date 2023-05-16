42c0fd5f63563d51fa73bd2be086b52d2eb91c8c
SiriRise <119751739+SiriRise@users.noreply.github.com>
2023-05-16 23:43:27 +0400

    Laba6_2.py

--- Laba6_2.py
+++ Laba6_2.py
@@ -5,43 +5,34 @@
 # целевая функция - лексемы с наибольшим числом согласных на четных местах
 
 word = "институт"
-lexems = set()
-def generate_lexems_no_cogl(word, lexems, prefix="", length=1, prev_vowel=False, prev_char=""):
-    if length == len(word):
-        if word not in lexems:
-            lexems.add(word)
-        return
+vowels = set("аеиоуыэюя")
+usedscharacters = set()
+def find_permutations(string, prev=None):
+    if not string:
+        yield string
+    else:
+        usedscharacters = set()
+        for i, ch in enumerate(string):
+            if prev is None or (prev not in vowels or ch not in vowels):
+                if ch not in usedscharacters:
+                    usedscharacters.add(ch)
+                    remainingscharacters = string[:i] + string[i + 1:]
+                    for permutation in find_permutations(remainingscharacters, ch):
+                        yield ch + permutation
 
-    for i, char in enumerate(word):
-        is_vowel = char in "бвгджзйклмнпрстфхцчшщ"
-        has_double_vowels = is_vowel and prev_vowel
-        if prev_char and is_vowel and prev_char in "бвгджзйклмнпрстфхцчшщ":
-            continue
-        if not has_double_vowels and prefix + char not in lexems:
-            lexems.add(prefix + char)
-        generate_lexems_no_cogl(word[:i] + word[i + 1:], lexems, prefix + char, length + 1, is_vowel, char)
+def count_consonants_on_odd_positions(string):
+    consonants = set("бвгджзйклмнпрстфхцчшщ")
+    count = sum(1 for i, ch in enumerate(string) if i % 2 == 0 and ch in consonants)
+    return count
 
-
-def max_consonants_on_even_indexes(lexems):
-    max_count = 0
-    max_lexems = []
-    for lexem in lexems:
-        count = sum(1 for i, char in enumerate(lexem) if i % 2 != 0 and char not in "аеёиоуыэюя" and i != 0)
-        #
-        if count > max_count:
-            max_count = count
-            max_lexems = [lexem]
-        elif count == max_count:
-            max_lexems.append(lexem)
-    return max_lexems, max_count
-
-
-
-generate_lexems_no_cogl(word, lexems)
-
-
-max_lexems, max_count = max_consonants_on_even_indexes(lexems)
-print(f"\nМаксимальное число согласных на четных местах: {max_count}")
-print("Лексемы с максимальным числом согласных на четных местах:")
-for i, lexem in enumerate(max_lexems, 1):
-    print(f"{i}. {lexem}")
+permutations = list(find_permutations(word))
+max_count = max(count_consonants_on_odd_positions(string) for string in permutations)
+if max_count == 0:
+    print("Cогласных на нечетных местах нет")
+else:
+    print("Максимальное число согласных на нечетных местах:",max_count)
+    i = 1
+    for string in permutations:
+        if count_consonants_on_odd_positions(string) == max_count:
+            print(f"{i}. {string}")
+            i+=1
