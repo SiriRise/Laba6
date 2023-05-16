7d5ffafa3bf3d8d64656f69a232171854374b1d7
SiriRise <119751739+SiriRise@users.noreply.github.com>
2023-05-16 10:36:24 +0400

    Laba6_1.py

--- Laba6_1.py
+++ Laba6_1.py
@@ -3,7 +3,7 @@
 # 1 часть – написать программу в соответствии со своим вариантом задания.
 
 word = "институт"
-
+lexems = set()
 def generate_lexems(word, lexems, prefix="", length=1):
     if length > 1 and prefix not in lexems:
         lexems.add(prefix)
@@ -11,12 +11,13 @@ def generate_lexems(word, lexems, prefix="", length=1):
         if word not in lexems:
             lexems.add(word)
         return
-    for i, char in enumerate(word):
-        generate_lexems(word[:i] + word[i + 1:], lexems, prefix + char, length + 1)
+    for i in range(len(word)):
+        char = word[i]
+        if i == 0 or word[i] != word[i-1]:
+            generate_lexems(word[:i] + word[i + 1:], lexems, prefix + char, length + 1)
 
-lexems = set()
+
 generate_lexems(word, lexems)
-
 print(f"Лексемы в слове '{word}':")
 for i, lexem in enumerate(sorted(lexems), 1):
     print(f"{i}. {lexem}")
